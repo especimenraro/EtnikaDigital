8a910aa089f340ffd2751210f7cd31a2212f6820
ricardo <especimenraro.cl@gmail.com>
2018-01-26 20:35:41 -0300

    textura tierra ok

--- js/cosmosEtnika.js
+++ js/cosmosEtnika.js
@@ -20,95 +20,94 @@ renderer.setClearColor(0x000000)
 renderer.setSize(window.innerWidth,window.innerHeight)
 renderer.shadowMapEnabled = true
 
-// AGREGA EJES PARA REFERENCIA
-var axes = new THREE.AxesHelper(20)
-scene.add(axes)
+// AGREGA EJES
 
-//AGREGA PLANO
- var textureGrass = new THREE.TextureLoader().load("./imagenes/grasslight-big.jpg");
-        textureGrass.wrapS = THREE.RepeatWrapping;
-        textureGrass.wrapT = THREE.RepeatWrapping;
-        textureGrass.repeat.set(2, 2);
-var  planeGeometry = new THREE.PlaneGeometry(60,20,1,1),
-			 planeMaterial = new THREE.MeshLambertMaterial({map:textureGrass}),
-			 plane = new THREE.Mesh(planeGeometry,planeMaterial)
-plane.rotation.x=-0.5*Math.PI
-plane.position.x = 15
-plane.position.y = 0
-plane.position.z = 0
-plane.receiveShadow = true
-scene.add(plane)
+var ejes = new THREE.AxesHelper(20,20,20)
+scene.add(ejes)
 
-// AGREGA CUBO
-
-var cubeGeometry = new THREE.BoxGeometry(4,4,4),
-			 cubeMaterial = new THREE.MeshLambertMaterial({color:0xff0000, wireframe: false}),
-			 cube = new THREE.Mesh(cubeGeometry,cubeMaterial)
-cube.castShadow = true
-cube.position.x = -4
-cube.position.y = 3
-cube.position.z = 0
-
-scene.add(cube)
 
 //AGREGA ESFERA
-var   sphereGeometry = new THREE.SphereGeometry(4,20,20),
-			 sphereMaterial = new THREE.MeshLambertMaterial({color:0x7777ff, wireframe: false}),
+var   sphereGeometry = new THREE.SphereGeometry(9,40,40),
+			 textureEarth =new  THREE.TextureLoader().load('./imagenes/tierra.jpg')
+			 sphereMaterial = new THREE.MeshLambertMaterial({map: textureEarth}),
 			 sphere = new THREE.Mesh(sphereGeometry,sphereMaterial)
 
 sphere.castShadow = false
-sphere.position.x = 7
-sphere.position.y = 4
+sphere.position.x = 0
+sphere.position.y = 0
 sphere.position.z = 0
-
+sphere.rotation.y = Math.PI
 scene.add(sphere)
 
-// AMBIENTE 
-/*
+//AMBIENTE 
+
 var ambiColor = "#ffffff";
        var ambientLight = new THREE.AmbientLight(ambiColor);
+       ambientLight.intensity = 0.5
         scene.add(ambientLight);
-*/
-// AGREGA LUZ
-var lampara = new THREE.SpotLight(0xffffff)
 
-lampara.position.x = 0
-lampara.position.y =10
-lampara.position.z =-60
-lampara.castShadow = true
-lampara.lookAt(plane)
-scene.add(lampara)
+// AGREGA SOL
+var sol = new THREE.DirectionalLight(0xffffff)
+
+sol.position.x = -15
+sol.position.y =0
+sol.position.z =-60
+sol.intensity = 2
+sol.castShadow = true
+scene.add(sol)
 
 var  textureFlare0 =new  THREE.TextureLoader().load('./imagenes/lensflare0.png'),
 			textureFlare3 =new  THREE.TextureLoader().load('./imagenes/lensflare3.png'),
 		    flareColor = new THREE.Color(0xffaacc),
-			 flare = new THREE.LensFlare(textureFlare0,350,0.0,THREE.AdditiveBlending,flareColor)
+			 flare = new THREE.LensFlare(textureFlare0,500,0.0,THREE.AdditiveBlending,flareColor)
 			 /*
 			  flare.add(textureFlare3, 60, 0.6, THREE.AdditiveBlending);
       flare.add(textureFlare3, 70, 0.7, THREE.AdditiveBlending);
         flare.add(textureFlare3, 120, 0.9, THREE.AdditiveBlending);
        flare.add(textureFlare3, 70, 1.0, THREE.AdditiveBlending);
 */
-flare.position.copy(lampara.position)
+flare.position.copy(sol.position)
 scene.add(flare)
+
+// AGREGA LUZ PARA EL LADO OSCURO
+
+var luzOscura = new THREE.SpotLight(0xffffff)
+
+luzOscura.position.x = 0
+luzOscura.position.y =0
+luzOscura.position.z =30
+luzOscura.intensity = 2
+luzOscura.castShadow = false
+scene.add(luzOscura)
+
 
 // POSICIONA LA CAMARA
 
-camera.position.x = -30
-camera.position.y = 20
-camera.position.z = 30
+camera.position.x = 20
+camera.position.y = 0
+camera.position.z = 50
 camera.lookAt(scene.position)
+
+var controls = new function () {
+            this.x = 0;
+            this.y =0;
+           
+                    };
+
+        var gui = new dat.GUI();
+        gui.add(controls, 'y',-60,60)
+			gui.add(controls, 'x',-60,60)
+      
 
 document.getElementById('WebGL-output').appendChild(renderer.domElement)
 renderScene()
 
 
 function renderScene() {
-	if (tiempo > 2) {tiempo =0} else {
-	tiempo += 0.1}
-cube.rotation.x += 0.1*Math.PI 
-sphere.position.y = 3 * Math.sin(tiempo*Math.PI) + 4
-
+if (tiempo>2) {tiempo =0} else {tiempo+=0.01}
+scene.rotation.y = tiempo*Math.PI
+sol.position.x = controls.x
+sol.position.y = controls.y
 stats.update()
 requestAnimationFrame(renderScene)
 renderer.render(scene,camera)
