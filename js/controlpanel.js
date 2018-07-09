fcdb901209bf7a3f178635e28064e3abf9f4dcc7
ricardo <especimenraro.cl@gmail.com>
2018-07-08 22:13:26 -0400

    integra controlpanel

---
+++ js/controlpanel.js
@@ -0,0 +1,161 @@
+var controlPanel = {
+	filtro: [],
+	url: '',
+	respuesta: [],
+	snippets:[],
+	imagenes: [],
+	titulos: [],
+	urls: [],
+	cantidadResultados: 0,
+	init: function () {
+		
+					$('#respuesta').hide()
+					
+					$('#x').click(function () {
+						
+						$('#interfaz-tierra').hide()
+					
+					})
+					
+					$('#botonBuscar').click(function () {
+						controlPanel.limpia()
+						
+						$(this).css('color','white')
+					})
+					
+					$('#botonBuscar').mousedown(function () {
+						$(this).css('color','red')
+					})
+					
+				
+					// EVENTO CLICK PARA BOTÓN BASURA
+					
+				$('.basura').click(function () {
+					var itemBorrar
+					for (key in $('.ui-selected')) {
+							controlPanel.borraItem($('.ui-selected')[key].innerHTML)			
+					} // FIN FOR 
+					$('.ui-selected').appendTo($('#origen'))
+					$('.movible').draggable({
+					containment: '.contenedor-contenido-tabs',
+					snap: true,
+					snapMode: 'inner',
+					revert: 'invalid'
+					
+				})
+									
+				})
+				
+				// USO DRAGGABLE Y DROPPABLE PARA LA TAB LISTA
+				
+				$('.movible').draggable({
+					containment: '.contenedor-contenido-tabs',
+					snap: true,
+					snapMode: 'inner',
+					revert: 'invalid'
+					
+				})
+				
+				$('#receptor').droppable({
+					acept: '.movible',
+					drop: function (event,ui) {
+						var categoria = ui.draggable[0].innerHTML 
+						var template = '<div class="movible ui-widget-content">' + ui.draggable[0].innerHTML + '</div>'
+						var child = ':contains(' + ui.draggable[0].innerHTML + ')'
+						$('#receptor').append(template)
+						$('#origen').find(child).remove()
+						$('.basura').show()
+						controlPanel.agregaItem(categoria)
+					}
+				})
+				
+				$('#receptor').selectable()
+	
+	}, // FIN INIT
+	limpia: function () {
+		controlPanel.snippets = []	
+		controlPanel.titulos = []	
+		controlPanel.imagenes = []	
+		controlPanel.urls = []	
+		controlPanel.creaBusqueda()
+		controlPanel.buscar()
+	}, // FIN LIMPIA
+	agregaItem: function (nuevoItem) {
+			controlPanel.filtro.push(nuevoItem)
+	}, // FIN AGREGA ITEM
+	
+	borraItem: function (item) {
+		
+			var itemTmp = []
+			for (key in controlPanel.filtro){
+				if (controlPanel.filtro[key] != item) {
+					itemTmp.push(controlPanel.filtro[key])				
+				}			
+			}
+			controlPanel.filtro = itemTmp
+	
+	},//FIN BORRA ITEM
+	
+	creaBusqueda: function () {
+	    
+	    var filtroTmp = controlPanel.filtro
+	    var busqueda = ''
+	    var fechaStart, fechaEnd
+	    for (key in filtroTmp) {
+					 busqueda += filtroTmp[key] + ' '   	
+	    	}
+	    fechaStart = $('#fechaInicio').val()
+	    fechaEnd = $('#fechaFin').val()
+	    controlPanel.url=busqueda
+	   
+	}, // FIN CREAURL
+	
+	buscar: function () {
+      google.search.cse.element.getElement('resultados').execute(controlPanel.url)
+		setTimeout(function () {
+			controlPanel.obtieneObjetos($('.gs-webResult'))
+			 
+		}, 2000)
+	  
+	}, // FIN BUSCAR
+	obtieneObjetos(objetos) {
+		
+		for (i=0;i<objetos.length-1 || i<10;i++) {
+			controlPanel.titulos.push(objetos[i].childNodes[0].innerText)
+			controlPanel.urls.push(objetos[i].childNodes[0].childNodes[0].children[0].href)	
+			controlPanel.snippets.push(objetos[i].childNodes[2].childNodes[0].childNodes[0].childNodes[1].innerText)
+			if (objetos[i].childNodes[2].childNodes[0].childNodes[0].childNodes[0].childNodes[0]) {
+			controlPanel.imagenes.push(objetos[i].childNodes[2].childNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[0].src)	
+			} else {
+				controlPanel.imagenes.push('../imagen/fondo_panel_categorias.png')
+			}
+			
+		}
+	
+	}, // FIN OBTIENE OBJETOS
+	obtieneSnippets(snippets) {
+	for (i=0;i<snippets.length-1;i++) {
+		controlPanel.snippets.push(snippets[i].innerText)	
+	}
+		
+	},
+	
+	obtieneImagenes(imagenes) {
+	for (i=0;i<imagenes.length-1;i++) {
+		controlPanel.imagenes.push(imagenes[i].src)	
+	}
+	},
+	
+	obtieneTitulos(titulos) {
+	for (i=0;i<titulos.length-1;i++) {
+		controlPanel.titulos.push(titulos[i].innerText)
+		controlPanel.urls.push(titulos[i].href)		
+	}
+	},
+	
+} // FIN CONTROL PANEL
+
+
+													
+
+
